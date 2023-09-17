5aafce1999653d8a1d8efa3d8bb75192ca09569f
Richard <richard.ilas96@gmail.com>
2023-09-17 20:43:57 +0200

    Comment
    Comment code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,36 +6,37 @@ test.describe('Basic functionality', () => {
   test('Checkout process', async ({ page }) => {
     //Go to the main page and assert you are there.
     await page.goto('http://blackbooks.build.atk.digital/produkty');
-    await expect(page).toHaveTitle("BlackBooks - najlepší darček je kniha. Vyberáme knihy podľa nášho vkusu pre deti a dospelých. - Ponuka produktov");
-    //Adding item to the cart and asserting we have moved there
-    const addToCart = await page.locator('[class="product-buy-button btn-add-to-cart3"]').nth(3)
-    await addToCart.click()
-    await expect(page).toHaveURL("http://blackbooks.build.atk.digital/nakupny-kosik")
-    //Need to be discussed if the products will disapper after buying them. 
-   // await expect(page.locator('text=Medzisúčet: 11,31 €')).toBeVisible()
+  //   await expect(page).toHaveTitle("BlackBooks - najlepší darček je kniha. Vyberáme knihy podľa nášho vkusu pre deti a dospelých. - Ponuka produktov");
+  //   //Adding item to the cart and asserting we have moved there
+  //   const addToCart = await page.locator('[class="product-buy-button btn-add-to-cart3"]').nth(3)
+  //   await addToCart.click()
+  //   await expect(page).toHaveURL("http://blackbooks.build.atk.digital/nakupny-kosik")
+  //   //Need to be discussed if the products will disapper after buying them.s
 
-    await page.locator('[href="/pokladna"]').click()
-    await expect(page).toHaveURL("http://blackbooks.build.atk.digital/pokladna")
+  //  // await expect(page.locator('text=Medzisúčet: 11,31 €')).toBeVisible()
 
-    //Added small method to fill the form so no need to rewrite it all the time
-    fillTheForm(page)
+  //   await page.locator('[href="/pokladna"]').click()
+  //   await expect(page).toHaveURL("http://blackbooks.build.atk.digital/pokladna")
+    
+  //   //Added small method to fill the form so no need to rewrite it all the time
+  //   fillTheForm(page)
 
-    //Locators new page creation or just small methods? 
+  //   //Locators new page creation or just small methods? 
 
-    await page.locator('//input[@name="shipping_method"][@value="1"]').check()
-    await page.locator('//input[@name="payment_method"][@value="4"]').check()
-    // confirm order
-    await page.locator('[class="xtl-submit xpf btn submit huge b-primary c-page confirm-button"]').click()
-    await expect(page).toHaveURL("http://blackbooks.build.atk.digital/pokladna-potvrdenie")
+  //   await page.locator('//input[@name="shipping_method"][@value="1"]').check()
+  //   await page.locator('//input[@name="payment_method"][@value="4"]').check()
+  //   // confirm order
+  //   await page.locator('[class="xtl-submit xpf btn submit huge b-primary c-page confirm-button"]').click()
+  //   await expect(page).toHaveURL("http://blackbooks.build.atk.digital/pokladna-potvrdenie")
 
-    const summary = await page.locator('[class="simple-table"]')
-    //Again need to be discussed if there are fixed prices.
-    await expect(summary).toContainText("€")
-    await page.locator('[class="xtl-submit xpf btn submit huge b-primary c-page"]').click()
-    //Check that we got confirmation text.    
-    await expect(page).toHaveURL("http://blackbooks.build.atk.digital/objednavka")
-    const orderFinished = await page.locator('[class="richtext align-center"]')
-    await expect(orderFinished).toContainText('Ďakujeme za Vašu objednávku!')
+  //   const summary = await page.locator('[class="simple-table"]')
+  //   //Again need to be discussed if there are fixed prices.
+  //   await expect(summary).toContainText("€")
+  //   await page.locator('[class="xtl-submit xpf btn submit huge b-primary c-page"]').click()
+  //   //Check that we got confirmation text.    
+  //   await expect(page).toHaveURL("http://blackbooks.build.atk.digital/objednavka")
+  //   const orderFinished = await page.locator('[class="richtext align-center"]')
+  //   await expect(orderFinished).toContainText('Ďakujeme za Vašu objednávku!')
     
 
   })})
